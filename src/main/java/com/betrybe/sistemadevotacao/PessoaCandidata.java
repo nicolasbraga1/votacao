25b8c158ad79794985e80b4b2cdab876a9520c7d
Nícolas <nicolasbrga01@gmail.com>
2023-08-26 19:37:28 -0300

    (Feat) Req 02 - Criando a classe PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,39 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * Cria a classe PessoaCandidata que herda de Pessoa.
+ */
+public class PessoaCandidata extends Pessoa {
+  private String nome;
+  private Integer numero;
+  private Integer votos;
 
+  /**
+   * Construtor da classe.
+   */
+  public PessoaCandidata(String nome, Integer numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public Integer getNumero() {
+    return numero;
+  }
+
+  public Integer getVotos() {
+    return votos;
+  }
+
+  public void setNumero(Integer numero) {
+    this.numero = numero;
+  }
+
+  public void setVotos(Integer votos) {
+    this.votos = votos;
+  }
+
+  private void receberVoto() {
+    votos++;
+  }
 }
