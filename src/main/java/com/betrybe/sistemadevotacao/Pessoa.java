293fd8ac6fa86d4a85d594c7427ee0e4113fdf45
Nícolas <nicolasbrga01@gmail.com>
2023-08-26 19:08:31 -0300

    (Feat) Req 1 - Criando classe abstrata Pessoa

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,16 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+/**
+ * Cria a classe abstrata Pessoa com atributo nome e seu getter e setter.
+ */
+public abstract class Pessoa {
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String setNome(String nome) {
+    return this.nome = nome;
+  }
 }
